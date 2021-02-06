f065d6a99d9a31e46cd8473849cfb806648d7a3d
Arpita Koner <ararpi12@gmail.com>
2021-02-06 13:28:04 +0530

    fourth commit

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-function changeGeom(element){
-    var geomtype= element.text
-    document.getElementById('dropdownMenuButton').innerText= geomtype
-}
 var raster = new ol.layer.Tile({
     source: new ol.source.OSM(),
     visible: true
@@ -16,82 +12,110 @@ var raster = new ol.layer.Tile({
              zoom: 7,
              maxZoom: 10,
              minZoom:4,
-             rotation: 0.5
+             
          })
          
     });
 
-    var features= new ol.Collection();
-    var featureOverlay= new ol.layer.Vector({
-        soure: new ol.source.Vector({features: features}),
-        style: new ol.style.Style({
-            color: 'rgba(255, 255, 255, 0.2)'
-        }),
-        stroke: new ol.style.Stroke({
-            color: '#ffcc33',
-            width: 2
-        }),
-        image: new ol.style.Circle({
-            radius:7,
-            fill: new ol.style.Fill({
-                color: '#ffcc33'
-            })
-        })
+    var features = new ol.Collection();
+    var featureOverlay = new ol.layer.Vector({
+  source: new ol.source.Vector({features: features}),
+  style: new ol.style.Style({
+    fill: new ol.style.Fill({
+      color: 'rgba(255, 255, 255, 0.2)'
+    }),
+    stroke: new ol.style.Stroke({
+      color: '#ffcc33',
+      width: 2
+    }),
+    image: new ol.style.Circle({
+      radius: 7,
+      fill: new ol.style.Fill({
+        color: '#ffcc33'
+      })
     })
+  })
+});
 featureOverlay.setMap(map);
 
 var modify = new ol.interaction.Modify({
+  features: features,
+  // the SHIFT key must be pressed to delete vertices, so
+  // that new vertices can be drawn at the same position
+  // of existing vertices
+  deleteCondition: function(event) {
+    return ol.events.condition.shiftKeyOnly(event) &&
+        ol.events.condition.singleClick(event);
+  }
+});
+map.addInteraction(modify);
+
+var typeSelect = document.getElementById('geometry');
+
+var draw; 
+var value = typeSelect.value;
+var geometryFunction;
+function addInteraction() { 
+if (value== 'Point') {
+    value= 'point'
+} else if (value== 'LineString') {
+    value= 'LineString'
+} else if (value== 'Polygon') {
+    value= 'point'
+} else if (value== 'Point') {
+    value= 'Polygon'
+} else if (value== 'Circle') {
+    value= 'Circle'
+} else if (value === 'Square') {
+    value = 'Circle';
+    geometryFunction = new ol.interaction.Draw.createRegularPolygon(4);
+  } else if (value === 'Box') {
+    value = 'Circle';
+    geometryFunction = new ol.interaction.Draw.createBox();
+  } else if (value === 'Star') {
+    value = 'Circle';
+    geometryFunction = function (coordinates, geometry) {
+      var center = coordinates[0];
+      var last = coordinates[coordinates.length - 1];
+      var dx = center[0] - last[0];
+      var dy = center[1] - last[1];
+      var radius = Math.sqrt(dx * dx + dy * dy);
+      var rotation = Math.atan2(dy, dx);
+      var newCoordinates = [];
+      var numPoints = 12;
+      for (var i = 0; i < numPoints; ++i) {
+        var angle = rotation + (i * 2 * Math.PI) / numPoints;
+        var fraction = i % 2 === 0 ? 1 : 0.5;
+        var offsetX = radius * fraction * Math.cos(angle);
+        var offsetY = radius * fraction * Math.sin(angle);
+        newCoordinates.push([center[0] + offsetX, center[1] + offsetY]);
+      }
+      newCoordinates.push(newCoordinates[0].slice());
+      if (!geometry) {
+        geometry = new Polygon([newCoordinates]);
+      } else {
+        geometry.setCoordinates([newCoordinates]);
+      }
+      return geometry;
+    };
+  }
+  draw = new ol.interaction.Draw({
     features: features,
-    // the SHIFT key must be pressed to delete vertices, so
-    // that new vertices can be drawn at the same position
-    // of existing vertices
-    deleteCondition: function(event) {
-      return ol.events.condition.shiftKeyOnly(event) &&
-          ol.events.condition.singleClick(event);
-    }
+    type: (typeSelect.value),
+    geometryFunction: geometryFunction
   });
-  map.addInteraction(modify);
-  
-  var typeselect= document.getElementById('dropdownMenuButton');
-   var draw;
-   function addInteraction(){
-       draw= new ol.interaction.Draw({
-           features: features,
-           type: (typeselect.value)
-       });
-       map.addInteraction(draw);
-    //    if( value== 'None') {
-    //        var geometryFunction;
-    //        if (value== 'Square') {
-    //            value= Circle;
-    //            geometryFunction= CreateRegularPolygon(4);
-    //        }
-    //        else if (value== 'Box'){
-    //            value = 'Circle';
-    //            geometryFunction = createBox();
-    //        } 
-    //        else if (value === 'Star') {
-    //           value = 'Circle';
-    //    }
-    //    }
-    }
-    typeselect.onChange = function(e) {
-        map.removeInteraction(draw);
-        addInteraction();
-      };
-      
-      addInteraction();
+  map.addInteraction(draw);
+}
 
 
-// var coordinate= document.getElementById('footer');
-// map.on('click', function(e){
-//     console.log(e.coordinate);
-//     var str= e.coordinate;
-//     coordinate.innerHTML(str);
-// })
-// map.on('click', function(evt){
-//     var lonlat = new ol.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
-//     var lon = lonlat[0];
-//     var lat = lonlat[1];
-//     alert("You clicked near lat lon: "+ lon.toFixed(6) + "  " + lat.toFixed(6));
-//   }); 
+
+//change the geometry type
+typeSelect.onchange = function(e) {
+  map.removeInteraction(draw);
+  addInteraction();
+};
+document.getElementById('undo').addEventListener('click', function () {
+    draw.removeLastPoint();
+  });
+
+addInteraction();
